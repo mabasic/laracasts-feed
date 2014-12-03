c0b9acdca8e39b427548eff8ef5f436bed11927e
Mario Basic <mario.basic@outlook.com>
2014-12-03 19:48:22 +0100

    Added lesson type to output.

--- app/controllers/Api/FeedController.php
+++ app/controllers/Api/FeedController.php
@@ -6,46 +6,54 @@ use Response;
 
 class FeedController extends ApiController {
 
-	CONST CACHE_DURATION = 60;//minutes
+    CONST CACHE_DURATION = 60;//minutes
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		if (Cache::has('response'))	return Cache::get('response');
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        if (Cache::has('response')) return Cache::get('response');
 
-		$lessons = Laracasts::lessons();
+        $lessons = Laracasts::lessons();
 
-		$output = $this->generateJSONForLessons($lessons);
+        $output = $this->generateJSONForLessons($lessons);
 
-		$header = $this->setCORSHeaders();
+        $header = $this->setCORSHeaders();
 
-		Cache::put('response', $response = Response::json($output, 200, $header), $this::CACHE_DURATION);
+        Cache::put('response', $response = Response::json($output, 200, $header), $this::CACHE_DURATION);
 
-		return $response;
-	}
+        return $response;
+    }
 
-	/**
-	 * @param $lessons
-	 * @return array
-	 */
-	private function generateJSONForLessons($lessons)
-	{
-		$output = [];
+    /**
+     * @param $lessons
+     * @return array
+     */
+    private function generateJSONForLessons($lessons)
+    {
+        $output = [];
 
-		foreach ($lessons as $lesson)
-		{
-			$output[] = [
-				'title'   => $lesson->title,
-				'summary' => $lesson->summary,
-				'link'    => $lesson->link
+        foreach ($lessons as $lesson)
+        {
+            $output[] = [
+                'title'   => $lesson->title,
+                'summary' => $lesson->summary,
+                'link'    => $lesson->link,
+                'type'    => $this->detectLessonOrSeries($lesson->link)
 			];
-		}
+        }
 
-		return $output;
-	}
+        return $output;
+    }
+
+    private function detectLessonOrSeries($string)
+    {
+        if(strpos($string, 'episodes') !== false) return 'lesson';
+
+        return 'serie';
+    }
 
 }
